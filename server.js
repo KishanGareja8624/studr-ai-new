0a7ff2328bca529200a8fa428623738b969b4ed9
KishanGareja8624 <kishangareja8624@gmail.com>
2024-12-04 12:20:43 +0530

    server error

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-import { createRequestHandler } from '@remix-run/vercel';
-import * as build from './build/index.js';
+const { createRequestHandler } = require("@remix-run/vercel");
+const build = require("./build");
 
-export default (req, res) => {
-  try {
-    return createRequestHandler({ build })(req, res);
-  } catch (error) {
-    console.error("Serverless function failed:", error);
-    res.status(500).send("Internal Server Error");
-  }
-};
+module.exports = createRequestHandler({ build, mode: process.env.NODE_ENV });
