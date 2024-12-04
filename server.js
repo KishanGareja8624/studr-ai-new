4050530cee74b1d9c0ac1597c0d9994125b712aa
KishanGareja8624 <kishangareja8624@gmail.com>
2024-12-04 12:03:25 +0530

    build error

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ import { createRequestHandler } from '@remix-run/vercel';
 import * as build from './build';
 
 export default (req, res) => {
-  return createRequestHandler({ build })(req, res);
+  try {
+    return createRequestHandler({ build })(req, res);
+  } catch (error) {
+    console.error("Serverless function failed:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
